4bf5deb907655f78fabf0c5cfe6252f935332e1c
Mx Nitro <95213910+Devilharsha@users.noreply.github.com>
2024-05-27 08:24:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    TELEGRAM_API = os.environ.get("TELEGRAM_API")
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    API_HASH = ("5a2e46d1e6deb1456c75aa743bc8e0e6")
+    BOT_TOKEN = ("7399145318:AAFv1VMcDVwPkQNElgdBBH5YJ-A3iw5G_i0")
+    TELEGRAM_API = 7931165
+    OWNER = ("6274275394")
+    OWNER_USERNAME = ("Devilharsha_2153")
+    PASSWORD = ("TMR")
+    DATABASE_URL = ("mongodb+srv://Filesstreambot:eHtqPEVohrO3YAS3@cluster0.hdxt929.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    LOGCHANNEL = ("-1002017090961")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
